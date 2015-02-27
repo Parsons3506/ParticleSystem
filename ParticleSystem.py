8d0d8f9994058374c1d7b591ab9f4d05aa09c685
Bradley Rothenberg <Brad@bradleyrothenberg.com>
2015-02-27 11:37:21 -0500

    draw sphere (file is big)

--- ParticleSystem.py
+++ ParticleSystem.py
@@ -6,13 +6,13 @@ import Rhino
 
 def main():
     #boxs = rs.ObjectsByName("box")
-    min = rs.ObjectsByName("min")
-    max = rs.ObjectsByName("max")
+    #min = rs.ObjectsByName("min")
+    #max = rs.ObjectsByName("max")
     
-    #box = rs.GetBox(prompt1="select box")
+    box = rs.GetBox(prompt1="select box")
     
-    minPoint = rs.coerce3dpoint(min[0])
-    maxPoint = rs.coerce3dpoint(max[0])
+    minPoint = box[0]
+    maxPoint = box[6]
     
     size = 5.0
     location = [0,0,0]
@@ -26,17 +26,22 @@ def main():
         y = random.uniform(minPoint[1],maxPoint[1])
         z = random.uniform(minPoint[2],maxPoint[2])
         
-        acc = random.uniform(-.01,-10)
-        
+        acc = random.uniform(-.01,-.5)
+        size = random.uniform(.1,2)
         tempParticle = Particle(size, [x,y,z], vel, [0,0,acc])
         tempParticle.setBoundingBox(boundingBox)
         particles.append(tempParticle)
     
-    for i in range(0,24):
-        loop(particles)
+    for i in range(0,200):
+        if(i%48):
+            drawSphere(particles)
+        else:
+            loop(particles)
     
     
     print"finished"
+
+
 
 def loop(particles):
     rs.EnableRedraw(False)
@@ -49,6 +54,13 @@ def loop(particles):
     rs.EnableRedraw(True)
     rs.EnableRedraw(False)
     rs.DeleteObjects(drawPoints)
+
+
+def drawSphere(particles):
+    for particle in particles:
+        tempPoint = particle.draw()
+        if tempPoint:
+            rs.AddSphere(rs.coerce3dpoint(tempPoint),particle._size)
 
 if(__name__ == "__main__"):
     main()
