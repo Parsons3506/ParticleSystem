f3959dae958e2d18a30ca28499cb4492a295c58b
Bradley Rothenberg <Brad@bradleyrothenberg.com>
2015-02-27 11:26:32 -0500

    Make It Rain

--- Particle.py
+++ Particle.py
@@ -1,5 +1,5 @@
 import rhinoscriptsyntax as rs
-
+import Rhino
 
 
 class Particle:
@@ -9,6 +9,20 @@ class Particle:
         self._loc = location
         self._vel = vel
         self._acc = acc
+        self._bBox = None
+        self._alive = True
+    
+    def setBoundingBox(self,boundingBox):
+        self._bBox = boundingBox
+        
+    def update(self):
+        self._vel = rs.VectorAdd(self._vel,self._acc)
+        self._loc = rs.VectorAdd(self._loc, self._vel)
+        if(self._bBox and not self._bBox.Contains(Rhino.Geometry.Point3d(self._loc))):
+            self._alive = False
     
     def draw(self):
-        rs.AddSphere(self._loc,self._size)
+        if(self._alive):
+            return rs.AddPoint(self._loc)
+        else:
+            return False
--- ParticleSystem.py
+++ ParticleSystem.py
@@ -1,7 +1,7 @@
 import rhinoscriptsyntax as rs
 from Particle import Particle
 import random
-
+import Rhino
 
 
 def main():
@@ -17,21 +17,35 @@ def main():
     acc = [0,0,0]
     
     particles = []
-    
+    boundingBox = Rhino.Geometry.BoundingBox(minPoint,maxPoint)
     for i in range(0,1000):
         x = random.uniform(minPoint[0],maxPoint[0])
         y = random.uniform(minPoint[1],maxPoint[1])
         z = random.uniform(minPoint[2],maxPoint[2])
         
+        acc = random.uniform(-.01,-10)
         
-        
-        particles.append(Particle(size, [x,y,z], vel, acc))
+        tempParticle = Particle(size, [x,y,z], vel, [0,0,acc])
+        tempParticle.setBoundingBox(boundingBox)
+        particles.append(tempParticle)
     
-    for particle in particles:
-        particle.draw()
+    for i in range(0,24):
+        loop(particles)
     
-    print"hello"
+    
+    print"finished"
 
+def loop(particles):
+    rs.EnableRedraw(False)
+    drawPoints = []
+    for particle in particles:
+        particle.update()
+        tempPoint = particle.draw()
+        if tempPoint:
+            drawPoints.append(tempPoint)
+    rs.EnableRedraw(True)
+    rs.EnableRedraw(False)
+    rs.DeleteObjects(drawPoints)
 
 if(__name__ == "__main__"):
     main()
